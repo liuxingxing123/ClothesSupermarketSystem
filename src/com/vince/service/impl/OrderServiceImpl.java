d3352f741dafadfd05141b796d805ffb14c0d1aa
liuxingxing <lxxlol@foxmail.com>
2019-06-18 20:49:06 +0800

    update product buy2

--- src/com/vince/service/impl/OrderServiceImpl.java
+++ src/com/vince/service/impl/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.vince.service.impl;
 import com.vince.bean.Order;
 import com.vince.service.OrderService;
 import com.vince.utils.BussinessException;
+import com.vince.utils.OrderIO;
 
 import java.util.List;
 
@@ -11,18 +12,19 @@ import java.util.List;
  * @date 2019-06-18 15:11
  */
 public class OrderServiceImpl implements OrderService {
+    private OrderIO orderIO = new OrderIO();
     @Override
     public void buyProduct(Order o) throws BussinessException {
-
+        orderIO.add(o);
     }
 
     @Override
     public List<Order> list() throws BussinessException {
-        return null;
+        return orderIO.list();
     }
 
     @Override
     public Order findById(int orderId) throws BussinessException {
-        return null;
+        return orderIO.findByOrderId(orderId);
     }
 }
